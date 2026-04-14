1d12d63158ccf9a324b09c9b0fe31bd5dd7eadc2
arthFinanceServices <arthfinanceservices@gmail.com>
2026-04-14 15:53:41 +0530

    Update AppRouter.jsx

--- Frontend/src/routes/AppRouter.jsx
+++ Frontend/src/routes/AppRouter.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Admin from "../components/Admin";
 import Employee from "../components/Employee";
 import EmployeeDetails from "../Employee Details/EmployeeDetails";
 import EmployeeDashboad from "../components/EmployeeDashboad";
+import DealerStatus from "../components/DealerStatus";
 
 import { AdminProtectedRoute } from "../components/AdminProtectedRoute";
 import { EmployeeProtectedRoute } from "../components/EmployeeProtectedRoute";
@@ -11,8 +12,13 @@ import { EmployeeProtectedRoute } from "../components/EmployeeProtectedRoute";
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/admin" element={<Admin />}></Route>
-      <Route path="/employee" element={<Employee />}></Route>
+      <Route path="/" element={<Navigate to="/admin" />} />
+
+      {/* Auth */}
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/employee" element={<Employee />} />
+
+      {/* Admin */}
       <Route
         path="/employeeDetails"
         element={
@@ -20,7 +26,9 @@ const AppRouter = () => {
             <EmployeeDetails />
           </AdminProtectedRoute>
         }
-      ></Route>
+      />
+
+      {/* Employee */}
       <Route
         path="/employee-dashboard"
         element={
@@ -28,7 +36,10 @@ const AppRouter = () => {
             <EmployeeDashboad />
           </EmployeeProtectedRoute>
         }
-      ></Route>
+      />
+
+      {/* ✅ Dealer Status (PUBLIC) */}
+      <Route path="/dealer-status" element={<DealerStatus />} />
     </Routes>
   );
 };
